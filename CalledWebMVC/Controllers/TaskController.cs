1d98db2095c2c4954f313ad3a20bbed26ee43bc7
Olvrenan <oliveirarenan339@gmail.com>
2021-04-18 20:42:39 -0300

    projeto criado e criação da pagina de cadastro de atividades

---
+++ CalledWebMVC/Controllers/TaskController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalledWebMVC.Controllers
+{
+    public class TaskController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ CalledWebMVC/Models/Task.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalledWebMVC.Models
+{
+    public class Task
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Datecreated { get; set; }
+        public DateTime DateDone { get; set; }
+        public string Assingn { get; set; }
+
+        public Task()
+        {
+        }
+
+        public Task(string title, DateTime dateCreated, DateTime dateDone)
+        {
+            Title = title;
+            Datecreated = dateCreated;
+            DateDone = dateDone;
+        }
+    }
+
+    
+}
